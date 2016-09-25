6ecdc577a992c3bbc387ff3558a54ad6ab697931
infamousbazooka <magnusfernandes1295@gmail.com>
2016-09-25 21:26:58 +0530

    Navigation fixed and preloader added to Event, Team, Schedule, and Contact

--- js/contact.js
+++ js/contact.js
@@ -1,2 +1,18 @@
 $('#map').css('height', $(window).height());
 $('.content').css('height', $(window).height());
+
+var minimumShowLoadingTimeReached = false;
+setTimeout(function() {minimumShowLoadingTimeReached = true;}, 3000);
+function preloader() {
+	if (minimumShowLoadingTimeReached) {
+		showContent();
+	} else {
+		setTimeout(function() {
+			preloader();
+		}, 500);
+	}
+}
+function showContent(){
+	$('body').addClass('loaded');
+}
+window.onload = preloader;
